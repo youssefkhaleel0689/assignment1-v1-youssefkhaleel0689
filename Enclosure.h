ad2ab305e9626102c298a9ae1911cf5eaa3a5e1c
youssefkhaleel0689 <youssef.khaleel@student.giu-uni.de>
2025-11-12 15:36:58 +0200

    Update Enclosure.h

--- Enclosure.h
+++ Enclosure.h
@@ -4,6 +4,20 @@
 #include "Animal.h"
 
 //define enclosure class here 
+class Enclosure :public Animal  {
+private:
+	Animal* ann;
+	int capacity;
+	int currentCount;
+public :
+	void addAnimal(Animal * a);
+	void displayAnimal();
+	Enclosure(Animal* ann,int capacity, int currentCount);
+	Enclosure();
+	~Enclosure();
+
+
+};
 
 
 
